17b7ad683871a12ef34d1219e9d347e729733620
Farrukh.Masood <Farrukh.Masood@FARRUKH-R90QXQXW>
2020-04-07 22:23:08 +0300

    updated the test

--- petstore/src/test/java/com/exam/petstore/testcases/Validate_UpdatePetInfo_Test.java
+++ petstore/src/test/java/com/exam/petstore/testcases/Validate_UpdatePetInfo_Test.java
@@ -3,6 +3,7 @@ package com.exam.petstore.testcases;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,46 +16,67 @@ import com.exam.petstore.autinterface.PetStoreClient;
 import com.exam.petstore.autobject.Pet;
 import com.exam.petstore.testdata.PetDataFactory;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+
 public class Validate_UpdatePetInfo_Test {
 	
-	PetDataFactory petDataFactory;
-	Pet pet;
-	Pet petResponse;
-	@Autowired
-	PetStoreClient petStoreClient;
-	
-	@Before
-	public void setup()
-	{
-		petDataFactory = new PetDataFactory();
-		pet = petDataFactory.getPetInfo();
-		pet.setId(300);
-		petResponse = petStoreClient.postPetInfo(pet);
-	}
+	  PetDataFactory petDataFactory;
+	  Pet pet;
+	  Pet petResponse;
+	  
+	  @Before
+	  public void setup()
+	  {
+		  RestAssured.baseURI = "https://petstore.swagger.io";
+		  petDataFactory = new PetDataFactory();
+		  pet = petDataFactory.getPetInfo();
+		 
+		 
+	  }
 	
 	@Test
 	public void validate_update_pet_info()
-	{
-		//@Given: A pet already added to the store
-		assertNotNull(petResponse);
+	{		
+		 petResponse =  RestAssured.given().body(pet).contentType(ContentType.JSON).request("PUT","/v2/pet").then().assertThat().extract().as(Pet.class);
+		 Response response = RestAssured.given().body(changepetInfo()).contentType(ContentType.JSON).request("PUT","/v2/pet");
+		 
+		 assertEquals("Wrong response status returned",200,response.getStatusCode());
+		 assertTrue(pet.equals(response.getBody().as(Pet.class)));
+						
+	}
+	
+	
+	@Test
+	public void validate_update_pet_info_validation_failure()
+	{		
+		 
+		 Response response = RestAssured.given().body("").contentType(ContentType.JSON).request("PUT","/v2/pet");
+		 
+		 assertEquals("Wrong response status returned",405,response.getStatusCode());
+						
+	}
+	
+	
+	@Test
+	public void validate_update_pet_info_invalid_pet_info()
+	{		
+		pet.setId(787878448);
 		
-		//@When: User update the pet status and update the store information
-		pet.setStatus("unavailable");
-		petStoreClient.updatePetInfo(pet);
-		
-		//@Then: The store information should be updated for the pet
-		Pet petUpdateResponse = getUpdatedResponse(pet.getId());
-		assertNotNull(petUpdateResponse);
-		assertTrue(petUpdateResponse.equals(pet));
+		 Response response = RestAssured.given().body(pet).contentType(ContentType.JSON).request("PUT","/v2/pet");
+		 
+		 assertEquals("Test for invalid request to update pet info failed",400,response.getStatusCode());
+						
+	}
+	
+
+	private Pet changepetInfo() {
+		 pet.setStatus("unavailable");
+		return pet;
 	}
 
-	private Pet getUpdatedResponse(Integer id) {
-		Pet petUpdateRespone = petStoreClient.getPetInfo(id.toString());
-		assertNotNull(petUpdateRespone);
-		return petUpdateRespone;
-	}
 
 }
